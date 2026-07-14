1df8b84adfbd3f99660c417487b95446043324e3
virendrapsamrat <49672748+virendrapsamrat@users.noreply.github.com>
2026-07-14 11:56:46 +0530

    fix: update jest

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,10 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^lucide-react-native$':
+      '<rootDir>/node_modules/lucide-react-native/dist/cjs/lucide-react-native.js',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|@reduxjs/toolkit|react-redux|redux|reselect|immer|@tanstack/.*))',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@react-navigation/.*|@reduxjs/toolkit|react-redux|redux|reselect|immer|@tanstack/.*|lucide-react-native))',
   ],
 };
